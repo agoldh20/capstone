7f6f8491022932821618617bc1a88bfc3ef2928c
agoldwater <dmgldwtr@gmail.com>
2017-09-28 21:30:32 -0500

    updated recipes and ingredients, added ingredient page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928004226) do
+ActiveRecord::Schema.define(version: 20170928185147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170928004226) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "substitute_connections", force: :cascade do |t|
