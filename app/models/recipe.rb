922ab217c6277afe085d04513d44b8e5c0cf4dd0
agoldwater <dmgldwtr@gmail.com>
2017-11-13 20:19:17 -0600

    I have no idea what the f diana did here

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,6 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :joint_categories
   has_many :categories, through: :joint_categories
-  belongs_to :user
 
   validates :title, uniqueness: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :recipes
 
   validates :first_name, presence: true
   validates :last_name, presence: true
